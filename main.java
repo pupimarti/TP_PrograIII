d5a06c4a41d337444c00d3b3adbea1fdaffbdbc0
Pupi Marti <42395985+pupimarti@users.noreply.github.com>
2019-11-14 16:40:38 -0300

    Add Dijkstra

--- main.java
+++ main.java
@@ -1,4 +1,7 @@
+import java.util.Vector;
+
 import grafo.GrafoDinamic;
+import grafo.NodoArista;
 
 
 public class main {
@@ -24,6 +27,7 @@ public class main {
 		arbol.agregarArista(6, 4, 2);
 		System.out.println("DFS:");
 		arbol.dephtFirstSearch(1, null);	
+		System.out.println();
 		System.out.println();
 	arbol = new GrafoDinamic();
 		arbol.agregarVertice(1);
@@ -51,9 +55,11 @@ public class main {
 		arbol.agregarArista(8, 9, 17);
 		arbol.agregarArista(9, 10, 3);
 		System.out.println("PRIM:");
+		arbol.PRIM(1, null, false);
+		
 
-		System.out.println();
-		/*arbol = new GrafoDinamic();
+		/*System.out.println();
+		arbol = new GrafoDinamic();
 		arbol.agregarVertice(1);
 		arbol.agregarVertice(2);
 		arbol.agregarVertice(3);
@@ -65,6 +71,35 @@ public class main {
 		arbol.agregarArista(3, 5, 10);
 		arbol.agregarArista(3, 4, 8);
 		arbol.agregarArista(4, 5, 3);*/
-		arbol.PRIM(1, null);
+
+		
+		arbol = new GrafoDinamic();
+		arbol.agregarVertice(1);
+		arbol.agregarVertice(2);
+		arbol.agregarVertice(3);
+		arbol.agregarVertice(4);
+		arbol.agregarVertice(5);
+		arbol.agregarVertice(6);
+		arbol.agregarVertice(7);
+		arbol.agregarArista(1, 2, 6);
+		arbol.agregarArista(1, 3, 7);
+		arbol.agregarArista(1, 4, 7);
+		arbol.agregarArista(2, 3, 2);
+		arbol.agregarArista(2, 6, 4);
+		arbol.agregarArista(3, 4, 1);
+		arbol.agregarArista(3, 6, 3);
+		arbol.agregarArista(3, 5, 4);
+		arbol.agregarArista(4, 5, 3);
+		arbol.agregarArista(5, 6, 3);
+		arbol.agregarArista(5, 7, 9);
+		arbol.agregarArista(6, 7, 6);
+		System.out.println();
+		System.out.println("DIJSKTRA:");
+		imprimirArreglo(arbol.Dijkstra(1, 7));
+	}
+	
+	private static void imprimirArreglo(Vector<NodoArista> aristas) {
+		for(int i = aristas.size() - 1; i >= 0; i--)
+			System.out.println("De:" + aristas.elementAt(i).getDesde() + ". Hasta:"+aristas.elementAt(i).getHasta()+". Peso:"+aristas.elementAt(i).getPeso());
 	}
 }
